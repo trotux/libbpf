56b023feccc80e62ecc982196e5f59bcd4984f1f
Anatoly Nikulin <anatoly.nikulin@gcorelabs.com>
2022-01-02 22:10:01 +0300

    Fix for C++ building

--- src/bpf.h
+++ src/bpf.h
@@ -346,7 +346,11 @@ LIBBPF_API int bpf_task_fd_query(int pid, int fd, __u32 flags, char *buf,
 				 __u32 *buf_len, __u32 *prog_id, __u32 *fd_type,
 				 __u64 *probe_offset, __u64 *probe_addr);
 
+#ifdef __cplusplus
+enum bpf_stats_type : int;
+#else
 enum bpf_stats_type; /* defined in up-to-date linux/bpf.h */
+#endif
 LIBBPF_API int bpf_enable_stats(enum bpf_stats_type type);
 
 struct bpf_prog_bind_opts {
